7ee36877b6ee4c69bfcf066dfe700710b834612b
NV Tuân <you@example.com>
2021-10-11 22:07:06 +0700

    init file

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Anh lỗi bé vợ xinh gái",
+  descriptionHeader:
+    "Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn em vì đồng ý",
+  descriptionModar:
+    'anh biết là em sẽ đồng ý lờ xin lỗi cảu anh mà ^^ </br> Anh yêu bé Vợ xing gai<3.Mãi Mãi Yêu Vợ) </br> "Anh rất là yêu vợ.Yêu vợ rất nhiều luôn </br>  Yêu vợ nhất trời luôn "',
+};
