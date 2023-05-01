29f71256ad25a51d9475f73e85efe8cd051f6e69
CaoLoc2002 <96274444+CaoLoc2002@users.noreply.github.com>
2023-05-02 03:16:04 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Anh lỗi bé vợ xinh gái",
+  titleHeader: "Anh xin lỗi bé vợ xinh gái",
   descriptionHeader:
     "Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
-    'anh biết là em sẽ đồng ý lờ xin lỗi củu anh mà ^^ </br> Anh yêu bé xing gai<3.Mãi Mãi Yêu) </br> "Anh rất là yêu vợ.Yêu vợ rất nhiều luôn </br>  Yêu vợ nhất trời luôn "',
+    'anh biết là em sẽ đồng ý lời xin lỗi của anh màa ^^ </br> Anh yêu vợ xing gai<3.Mãi Mãi Yêu) </br> "Anh rất là yêu vợ.Yêu rất nhiều luôn </br>  Yêu bé nhất trời :33 "',
 };
